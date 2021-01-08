76a1d9744efdf90bb8c881202b1b32a7ef3ee8ed
Errol Cleary <errol.cleary@gmail.com>
2021-01-08 15:09:02 -0800

    Implement Mongoose schema

--- database/index.js
+++ database/index.js
@@ -1,16 +1,59 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+const Schema = mongoose.Schema;
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+const port = '27017';
+const host = 'localhost';
+const database = 'fetcher';
+const uri = `mongodb://${host}:${port}/${database}`;
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+};
+
+let connection;
+mongoose.connect(uri, options, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    module.exports.connection = connection = mongoose.connection;
+    console.log(`Successfully connected to MongoDB database ${database} with ID ${connection.id}`);
+    connection.on('error', console.error);
+    process.on(`SIGINT`, () => {
+      const {pid} = process;
+      connection.close();
+      console.log(`\nMongoDB database ${database} connection has been closed`);
+      console.log(`About to exit Node process ${pid}`);
+      process.exit();
+    });
+  }
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+const repoSchema = new Schema({
+  repoId: {type: String, required: true},
+  info: {
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    url: {type: String, required: true}
+  },
+  owner: {
+    userId: {type: String, required: true},
+    username: {type: String, required: true}
+  },
+  stats: {
+    stargazersCount: {type: Number, required: true},
+    watchersCount: {type: Number, required: true},
+    forksCount: {type: Number, required: true}
+  }
+});
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+module.exports.Repo = mongoose.model('Repo', repoSchema);
 
-module.exports.save = save;
+// let save = (/* TODO */) => {
+//   // TODO: Your code here
+//   // This function should save a repo or repos to
+//   // the MongoDB
+// }
+
+// module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let app = express();
+const database = require('../database');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
